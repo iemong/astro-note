0c66a881956a1dc5ade2aad9db0faee1891c077d
iemong <kiyoshidainagon@gmail.com>
2022-08-31 09:57:40 +0900

    reactの環境を追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 import svgr from 'vite-plugin-svgr';
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind()],
+  integrations: [svelte(), tailwind(), react()],
   vite: {
     plugins: [svgr()]
   }
