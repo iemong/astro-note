d66fa080d69c961d8482d1187d7a65f5ad3127ed
iemong <kiyoshidainagon@gmail.com>
2022-09-06 09:39:43 +0900

    fix

---
+++ src/components/molecules/Like/index.tsx
@@ -0,0 +1,10 @@
+import './index.css'
+
+export const Like = () => {
+    return (
+        <div className="flex gap-2 items-center -mt-2 text-[#a8abb1]">
+            <span className="like-icon"></span>
+            <span className="leading-none">9</span>
+        </div>
+    )
+}
